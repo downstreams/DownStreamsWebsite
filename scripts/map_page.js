707777f17a151be9c61b54b11c17b6d34a2cdc55
simonredding <simon@redding.name>
2014-05-22 17:33:12 +0100

    added bootleaf demo map

---
+++ scripts/map_page.js
@@ -0,0 +1,229 @@
+var map, elvSearch = [];
+
+/* Basemap Layers */
+var mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  subdomains: ["otile1", "otile2", "otile3", "otile4"],
+  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+});
+var mapquestOAM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+  maxZoom: 18,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+});
+var mapquestHYB = L.layerGroup([L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+  maxZoom: 18,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"]
+}), L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/hyb/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+  attribution: 'Labels courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+})]);
+
+/* Overlay Layers */
+var elv = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/car2.png",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.name,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.name + "</td></tr>" + "<tr><th>Address: </th><td>" + feature.properties.address + "<tr><th>Postcode: </th><td>" + feature.properties.postcode + "</td></tr><tr><th>Phone: </th><td>" + feature.properties.phone + "</td></tr>" + "<table>";
+
+      if (document.body.clientWidth <= 767) {
+        layer.on({
+          click: function (e) {
+            $("#feature-title").html(feature.properties.name + ", " + feature.properties.town);
+            $("#feature-info").html(content);
+            $("#featureModal").modal("show");
+          }
+        });
+
+      } else {
+        layer.bindPopup(content, {
+          maxWidth: "auto",
+          closeButton: false
+        });
+      }
+      elvSearch.push({
+        name: layer.feature.properties.name,
+        source: "Scrapyards",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+
+$.getJSON("data/elv.geojson", function (data) {
+  elv.addData(data);
+});
+
+map = L.map("map", {
+  zoom: 8,
+  center: [53.7, -2.1],
+  layers: [mapquestOSM]
+});
+
+/* var markers = L.markerClusterGroup(); */
+
+map.addLayer(elv);
+/* map.addLayer(markers); */
+
+/* Larger screens get expanded layer control */
+if (document.body.clientWidth <= 767) {
+  var isCollapsed = true;
+} else {
+  var isCollapsed = false;
+}
+
+var baseLayers = {
+  "Street Map": mapquestOSM,
+  "Aerial Imagery": mapquestOAM,
+  "Imagery with Streets": mapquestHYB
+};
+
+var overlays = {
+  "<img src='assets/car2.png' width='24' height='28'>&nbsp;Active Projects": elv
+};
+
+var layerControl = L.control.layers(baseLayers, overlays, {
+  collapsed: isCollapsed
+}).addTo(map);
+
+/* Add overlay layers to map after defining layer control to preserver order */
+
+/* map.fitBounds(elv.getBounds()); */
+/* map.addLayer(theaters); */
+
+/*
+var sidebar = L.control.sidebar("sidebar", {
+  closeButton: true,
+  position: "left"
+}).addTo(map);
+*/
+
+/* Highlight search box text on click */
+$("#searchbox").click(function () {
+  $(this).select();
+});
+
+/* Typeahead search functionality */
+$(document).one("ajaxStop", function () {
+  map.fitBounds(elv.getBounds());
+  $("#loading").hide();
+
+  var elvBH = new Bloodhound({
+    name: "Scrapyards",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: elvSearch,
+    limit: 10
+  });
+
+  var geonamesBH = new Bloodhound({
+    name: "GeoNames",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: "http://api.geonames.org/searchJSON?username=bootleaf&featureClass=P&maxRows=5&countryCode=US&name_startsWith=%QUERY",
+      filter: function (data) {
+        return $.map(data.geonames, function (result) {
+          return {
+            name: result.name + ", " + result.adminCode1,
+            lat: result.lat,
+            lng: result.lng,
+            source: "GeoNames"
+          };
+        });
+      },
+      ajax: {
+        beforeSend: function (jqXhr, settings) {
+          settings.url += "&east=" + map.getBounds().getEast() + "&west=" + map.getBounds().getWest() + "&north=" + map.getBounds().getNorth() + "&south=" + map.getBounds().getSouth();
+          $("#searchicon").removeClass("fa-search").addClass("fa-refresh fa-spin");
+        },
+        complete: function (jqXHR, status) {
+          $('#searchicon').removeClass("fa-refresh fa-spin").addClass("fa-search");
+        }
+      }
+    },
+    limit: 10
+  });
+  elvBH.initialize();
+  geonamesBH.initialize();
+
+  /* instantiate the typeahead UI */
+  $("#searchbox").typeahead({
+    minLength: 3,
+    highlight: true,
+    hint: false
+  }, {
+    name: "Scrapyards",
+    displayKey: "name",
+    source: elvBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'>Scrapyards</h4>"
+    }
+  }, 
+  {
+    name: "GeoNames",
+    displayKey: "name",
+    source: geonamesBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/globe.png' width='25' height='25'>&nbsp;GeoNames</h4>"
+    }
+  }).on("typeahead:selected", function (obj, datum) {
+    if (datum.source === "Scrapyards") {
+      if (!map.hasLayer(elv)) {
+        map.addLayer(elv);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+
+    if (datum.source === "GeoNames") {
+      map.setView([datum.lat, datum.lng], 14);
+    }
+    if ($(".navbar-collapse").height() > 50) {
+      $(".navbar-collapse").collapse("hide");
+    }
+  }).on("typeahead:opened", function () {
+    $(".navbar-collapse.in").css("max-height", $(document).height() - $(".navbar-header").height());
+    $(".navbar-collapse.in").css("height", $(document).height() - $(".navbar-header").height());
+  }).on("typeahead:closed", function () {
+    $(".navbar-collapse.in").css("max-height", "");
+    $(".navbar-collapse.in").css("height", "");
+  });
+  $(".twitter-typeahead").css("position", "static");
+  $(".twitter-typeahead").css("display", "block");
+});
+
+/* Placeholder hack for IE */
+if (navigator.appName == "Microsoft Internet Explorer") {
+  $("input").each(function () {
+    if ($(this).val() === "" && $(this).attr("placeholder") !== "") {
+      $(this).val($(this).attr("placeholder"));
+      $(this).focus(function () {
+        if ($(this).val() === $(this).attr("placeholder")) $(this).val("");
+      });
+      $(this).blur(function () {
+        if ($(this).val() === "") $(this).val($(this).attr("placeholder"));
+      });
+    }
+  });
+}
