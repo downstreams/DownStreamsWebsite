fbefa7ce0fbe3484bcd7fd09db42fb3e9d47db1e
simonredding <simon@redding.name>
2014-05-22 15:39:48 +0100

    tidied up

---
+++ scripts/projects_map.js
@@ -0,0 +1,456 @@
+var map, projectSearch = [], nafraSearch = [];
+/*    theaterSearch = [],
+    museumSearch = []; */
+
+/* Basemap Layers */
+var mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  subdomains: ["otile1", "otile2", "otile3", "otile4"],
+  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+});
+var mapquestOAM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+  maxZoom: 18,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+});
+var mapquestHYB = L.layerGroup([L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+  maxZoom: 18,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"]
+}), L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/hyb/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+  attribution: 'Labels courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+})]);
+
+/* Overlay Layers */
+var projects = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/img/ea-logo1.jpg",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.PROJECT,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.PROJECT + "</td></tr>" + "<tr><th>Organisation: </th><td>" + feature.properties.ORGANISATION + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.WEBSITE + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
+
+      if (document.body.clientWidth <= 767) {
+        layer.on({
+          click: function (e) {
+            $("#feature-title").html(feature.properties.PROJECT);
+            $("#feature-info").html(content);
+            $("#featureModal").modal("show");
+          }
+        });
+
+      } else {
+        layer.bindPopup(content, {
+          maxWidth: "auto",
+          closeButton: false
+        });
+      }
+      projectSearch.push({
+        name: layer.feature.properties.PROJECT,
+        source: "Projects",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+
+$.getJSON("data/projects.geojson", function (data) {
+  projects.addData(data);
+});
+
+var nafra = L.geoJson(null, {
+  style: function (feature) {
+    if (feature.properties.RISKCODE === "Low") {
+      return {
+        color: "#1a9641",
+        weight: 3,
+        opacity: 1
+      };
+    }
+    if (feature.properties.RISKCODE === "Moderate") {
+      return {
+        color: "#ffffbf",
+        weight: 3,
+        opacity: 1
+      };
+    }
+    if (feature.properties.RISKCODE === "Significant") {
+      return {
+        color: "#d7191c",
+        weight: 3,
+        opacity: 1
+      };
+    }
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Catchment</th><td>" + feature.properties.CATCHAREA + "</td></tr>" + "<tr><th>Risk</th><td>" + feature.properties.RISKCODE + "</td></tr>" + "<table>";
+      if (document.body.clientWidth <= 767) {
+        layer.on({
+          click: function (e) {
+            $("#feature-title").html(feature.properties.CATCHAREA);
+            $("#feature-info").html(content);
+            $("#featureModal").modal("show");
+          }
+        });
+
+      } else {
+        layer.bindPopup(content, {
+          maxWidth: "auto",
+          closeButton: false
+        });
+      }
+      nafraSearch.push({
+      name: layer.feature.properties.CATCHAREA,
+      source: "NAFRA",
+      id: L.stamp(layer),
+      lat: layer.feature.geometry.coordinates[1],
+      lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+    layer.on({
+      mouseover: function (e) {
+        var layer = e.target;
+        layer.setStyle({
+          weight: 3,
+          color: "#00FFFF",
+          opacity: 1
+        });
+        if (!L.Browser.ie && !L.Browser.opera) {
+          layer.bringToFront();
+        }
+      },
+      mouseout: function (e) {
+        nafra.resetStyle(e.target);
+      }
+    });
+  }
+});
+$.getJSON("data/nafra_wgs84.geojson", function (data) {
+  nafra.addData(data);
+});
+
+/*
+var theaters = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/img/theater.png",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.NAME,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADDRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
+
+      if (document.body.clientWidth <= 767) {
+        layer.on({
+          click: function (e) {
+            $("#feature-title").html(feature.properties.NAME);
+            $("#feature-info").html(content);
+            $("#featureModal").modal("show");
+          }
+        });
+
+      } else {
+        layer.bindPopup(content, {
+          maxWidth: "auto",
+          closeButton: false
+        });
+      }
+      theaterSearch.push({
+        name: layer.feature.properties.NAME,
+        source: "Theaters",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+$.getJSON("data/DOITT_THEATER_01_13SEPT2010.geojson", function (data) {
+  theaters.addData(data);
+});
+
+var museums = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/img/museum.png",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.NAME,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
+      if (document.body.clientWidth <= 767) {
+        layer.on({
+          click: function (e) {
+            $("#feature-title").html(feature.properties.NAME);
+            $("#feature-info").html(content);
+            $("#featureModal").modal("show");
+          }
+        });
+
+      } else {
+        layer.bindPopup(content, {
+          maxWidth: "auto",
+          closeButton: false
+        });
+      }
+      museumSearch.push({
+        name: layer.feature.properties.NAME,
+        source: "Museums",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+$.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
+  museums.addData(data);
+});
+*/
+
+map = L.map("map", {
+  zoom: 10,
+  /* center: [40.702222, -73.979378], */
+  center: [51.5, -0.89],
+  layers: [mapquestOSM]
+});
+
+/* Larger screens get expanded layer control */
+if (document.body.clientWidth <= 767) {
+  var isCollapsed = true;
+} else {
+  var isCollapsed = false;
+}
+
+var baseLayers = {
+  "Street Map": mapquestOSM,
+  "Aerial Imagery": mapquestOAM,
+  "Imagery with Streets": mapquestHYB
+};
+
+var overlays = {
+  /* "Projects": projects , */
+  "Flood Risk": nafra /*,
+  "<img src='assets/img/theater.png' width='24' height='28'>&nbsp;Theaters": theaters,
+  "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museums */
+};
+
+var layerControl = L.control.layers(baseLayers, overlays, {
+  collapsed: isCollapsed
+}).addTo(map);
+
+/* Add overlay layers to map after defining layer control to preserver order */
+map.addLayer(projects);
+map.addLayer(nafra);
+
+var sidebar = L.control.sidebar("sidebar", {
+  closeButton: true,
+  position: "left"
+}).addTo(map);
+
+/* Highlight search box text on click */
+$("#searchbox").click(function () {
+  $(this).select();
+});
+
+/* Typeahead search functionality */
+$(document).one("ajaxStop", function () {
+  map.fitBounds(projects.getBounds());
+  $("#loading").hide();
+
+  var projectsBH = new Bloodhound({
+    name: "Projects",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: projectSearch,
+    limit: 10
+  });
+
+  var nafraBH = new Bloodhound({
+    name: "Flood Risk",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: nafraSearch,
+    limit: 10
+  });
+
+  /* 
+  var museumsBH = new Bloodhound({
+    name: "Museums",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: museumSearch,
+    limit: 10
+  });
+  */
+
+  var geonamesBH = new Bloodhound({
+    name: "GeoNames",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: "http://api.geonames.org/searchJSON?username=bootleaf&featureClass=P&maxRows=5&countryCode=US&name_startsWith=%QUERY",
+      filter: function (data) {
+        return $.map(data.geonames, function (result) {
+          return {
+            name: result.name + ", " + result.adminCode1,
+            lat: result.lat,
+            lng: result.lng,
+            source: "GeoNames"
+          };
+        });
+      },
+      ajax: {
+        beforeSend: function (jqXhr, settings) {
+          settings.url += "&east=" + map.getBounds().getEast() + "&west=" + map.getBounds().getWest() + "&north=" + map.getBounds().getNorth() + "&south=" + map.getBounds().getSouth();
+          $("#searchicon").removeClass("fa-search").addClass("fa-refresh fa-spin");
+        },
+        complete: function (jqXHR, status) {
+          $('#searchicon').removeClass("fa-refresh fa-spin").addClass("fa-search");
+        }
+      }
+    },
+    limit: 10
+  });
+  projectsBH.initialize();
+  nafraBH.initialize();
+
+  /*
+  theatersBH.initialize();
+  museumsBH.initialize();
+  */
+  geonamesBH.initialize();
+
+  /* instantiate the typeahead UI */
+  $("#searchbox").typeahead({
+    minLength: 3,
+    highlight: true,
+    hint: false
+  }, {
+    name: "Projects",
+    displayKey: "name",
+    source: projectsBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'>Projects</h4>"
+    }
+  }, 
+  {
+    name: "Catchment",
+    displayKey: "Catchment",
+    source: nafraBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'>Catchment</h4>"
+    }
+  }, /* {
+    name: "Museums",
+    displayKey: "name",
+    source: museumsBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums</h4>"
+    }
+  }, */
+  {
+    name: "GeoNames",
+    displayKey: "name",
+    source: geonamesBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/globe.png' width='25' height='25'>&nbsp;GeoNames</h4>"
+    }
+  }).on("typeahead:selected", function (obj, datum) {
+    if (datum.source === "Projects") {
+      if (!map.hasLayer(projects)) {
+        map.addLayer(projects);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+      
+    if (datum.source === "NAFRA") {
+      if (!map.hasLayer(nafra)) {
+        map.addLayer(nafra);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+    /* 
+    if (datum.source === "Museums") {
+      if (!map.hasLayer(museums)) {
+        map.addLayer(museums);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+    */
+
+    if (datum.source === "GeoNames") {
+      map.setView([datum.lat, datum.lng], 14);
+    }
+    if ($(".navbar-collapse").height() > 50) {
+      $(".navbar-collapse").collapse("hide");
+    }
+  }).on("typeahead:opened", function () {
+    $(".navbar-collapse.in").css("max-height", $(document).height() - $(".navbar-header").height());
+    $(".navbar-collapse.in").css("height", $(document).height() - $(".navbar-header").height());
+  }).on("typeahead:closed", function () {
+    $(".navbar-collapse.in").css("max-height", "");
+    $(".navbar-collapse.in").css("height", "");
+  });
+  $(".twitter-typeahead").css("position", "static");
+  $(".twitter-typeahead").css("display", "block");
+});
+
+/* Placeholder hack for IE */
+if (navigator.appName == "Microsoft Internet Explorer") {
+  $("input").each(function () {
+    if ($(this).val() === "" && $(this).attr("placeholder") !== "") {
+      $(this).val($(this).attr("placeholder"));
+      $(this).focus(function () {
+        if ($(this).val() === $(this).attr("placeholder")) $(this).val("");
+      });
+      $(this).blur(function () {
+        if ($(this).val() === "") $(this).val($(this).attr("placeholder"));
+      });
+    }
+  });
+}
